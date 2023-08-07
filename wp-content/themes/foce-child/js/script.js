8c1b1ab6ebf578d4fafe0410857aa7811142c563
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-08-07 17:51:43 +0200

    Quelques tests

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,1 +1,18 @@
 console.log("bonjour")
+// document.addEventListener('DOMContentLoaded', (event) => {
+ 
+//     const observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//           const titres = entry.target.querySelector("h1,h2,h3");
+    
+//           if (entry.isIntersecting) {
+//             titres.classList.add('toAnim');
+//             return; 
+//           }
+    
+//           titres.classList.remove('toAnim');
+//         });
+//       });
+    
+//       observer.observe(document.querySelector('section'));
+//     });
