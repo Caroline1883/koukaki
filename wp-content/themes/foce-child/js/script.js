833ec1c438e5afc24572ac5c0c39998381bac2fe
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-08-09 12:47:27 +0200

    Etape 1 - Fade In par section

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,4 +1,24 @@
 console.log("bonjour")
+
+
+// Fade-in pour chaque section
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('fade-in-section')
+            return;
+            }
+    });        
+    });
+
+observer.observe(document.querySelector('.banner'));
+observer.observe(document.querySelector('.story'));
+observer.observe(document.querySelector('#studio'));
+
+
+
+
+
 // document.addEventListener('DOMContentLoaded', (event) => {
  
 //     const observer = new IntersectionObserver(entries => {
