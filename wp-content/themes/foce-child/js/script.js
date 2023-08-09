2e6b7f295a52577caaf5cc72d9ad96f435b9d666
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-08-09 17:44:16 +0200

    Etape 1 - Essai gestion logo

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -16,6 +16,26 @@ observer.observe(document.querySelector('.story'));
 observer.observe(document.querySelector('#studio'));
 
 
+//parallax
+
+const logo = document.querySelector('.banner img');
+
+const observerLogo = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            logo.classList.add('img-relative')
+            return;
+            }
+    });        
+    });
+
+observerLogo.observe(document.querySelector('.story h2'));
+
+const parallax = document.querySelector('.banner');
+window.addEventListener('scroll',() =>{
+    parallax.style.backgroundPositionY = -window.scrollY + "px"
+});
+
 
 
 
