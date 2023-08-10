7b82ab2561b276bded1838e1d3d0d29cb7b56699
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-08-10 06:39:58 +0200

    Etape 1 - Ajout Templates part Oscar

---
+++ wp-content/themes/foce-child/template-parts/oscar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part created for the oscar nomination 
+ *
+ * 
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+
+<section class="oscar">
+    <div>
+    Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !
+    </div>
+   <img src="<?php echo get_stylesheet_directory_uri() . '/assets/oscar.png'; ?> " alt="nomination">
+</section>
