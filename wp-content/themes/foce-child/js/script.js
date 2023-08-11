9752cb717ef1923897892aabbad7d57f8998c75e
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-08-11 17:55:08 +0200

    Mentorat

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,6 +1,3 @@
-console.log("bonjour")
-
-
 // Fade-in pour chaque section
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -18,37 +15,37 @@ observer.observe(document.querySelector('#studio'));
 
 //parallax
 
-const logo = document.querySelector('.banner img');
+// const logo = document.querySelector('.banner img');
 
-const observerLogo = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting){
-            logo.classList.add('img-relative')
-            return;
-            }
-    });        
-    });
+// const observerLogo = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting){
+//             logo.classList.add('img-relative')
+//             return;
+//             }
+//     });        
+//     });
 
-observerLogo.observe(document.querySelector('.story h2'));
+// observerLogo.observe(document.querySelector('.story h2'));
 
-const parallax = document.querySelector('.banner');
-window.addEventListener('scroll',() =>{
-    parallax.style.backgroundPositionY = window.scrollY + "px"
-});
+// const parallax = document.querySelector('.banner');
+// window.addEventListener('scroll',() =>{
+//     parallax.style.backgroundPositionY = -window.scrollY + "px"
+// });
 
 
 //Animation titre
 
-const observerTitle = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting){
-            entry.target.classList.add('animation-titre')
-            return;
-            }
-    });        
-    });
+// const observerTitle = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting){
+//             entry.target.classList.add('animation-titre')
+//             return;
+//             }
+//     });        
+//     });
 
-observerTitle.observe(document.querySelector('h2'));
-observerTitle.observe(document.querySelector('h3'));
+// observerTitle.observe(document.querySelector('h2'));
+// observerTitle.observe(document.querySelector('h3'));
 
 
